f3c7cda85a2a6e40d3416b89052960a0f9a325ac
idoSocha <idosocha88@gmail.com>
2023-06-01 20:29:37 +0300

    single Vacation

--- frontend/src/Components/Pages/MainPage/MainPage.tsx
+++ frontend/src/Components/Pages/MainPage/MainPage.tsx
@@ -1,11 +1,36 @@
 import "./MainPage.css";
 import { NavLink } from "react-router-dom";
 import SingleVacation from "../../Vacations/SingleVacation/SingleVacation";
+import { useState } from "react";
+import axios from "axios";
+import { log } from "console";
 
 function MainPage(): JSX.Element {
+  const [vacationList, setList] = useState([]);
+
+  axios
+    .get("http://localhost:4000/api/v1/vacations/vacationList")
+    .then((response) => {
+      setList(response.data);
+    });
+  // setList(vacationData.data);
+
   return (
-    <div className="Vacations">
-      <SingleVacation />
+    <div className="MainPage">
+      <div className="VacationList">
+        {vacationList.map((item) => (
+          <SingleVacation
+            key={item.vacation_code}
+            file_img_name={item.file_img_name}
+            destination={item.destination}
+            description={item.description}
+            start_date={item.start_date}
+            end_date={item.end_date}
+            price={item.price}
+          />
+        ))}
+      </div>
+
       <NavLink to="/logintemp">loginTemp</NavLink>
       <br />
       <NavLink to="/login">login</NavLink>
--- frontend/src/Components/Vacations/SingleVacation/SingleVacation.tsx
+++ frontend/src/Components/Vacations/SingleVacation/SingleVacation.tsx
@@ -8,26 +8,42 @@ import {
   Typography,
 } from "@mui/material";
 import "./SingleVacation.css";
+import moment from "moment";
 
-function SingleVacation(): JSX.Element {
+interface vacationProps {
+  vacation_code: number;
+  destination: string;
+  description: string;
+  start_date: Date;
+  end_date: Date;
+  price: number;
+  file_img_name: string;
+}
+function SingleVacation(props: vacationProps): JSX.Element {
   return (
     <div className="SingleVacation">
       <Box width="300px">
         <Card variant="outlined">
-          <CardMedia
-            component="img"
-            height="200px"
-            image="https:source.unsplash.com/random"
-          />
+          <CardMedia component="img" height="200px" src={props.file_img_name} />
           <CardContent>
-            <Typography variant="body2">hey</Typography>
+            <Typography variant="body2">{props.destination}</Typography>
+            <Typography variant="body2">{props.description}</Typography>
+            <Typography variant="body2">
+              from:
+              {moment(props.start_date).format("DD-MM-YYYY")}
+            </Typography>{" "}
+            <Typography variant="body2">
+              till:
+              {moment(props.end_date).format("DD-MM-YYYY")}
+            </Typography>
+            <Typography variant="body2">{props.price}€</Typography>
             <CardActions>
-              <Button size="small">$40</Button>
+              <Button size="small">Order Now!</Button>
             </CardActions>
           </CardContent>
         </Card>
       </Box>
-      <Box width="300px">
+      {/* <Box width="300px">
         <Card variant="outlined">
           <CardMedia
             component="img"
@@ -56,7 +72,7 @@ function SingleVacation(): JSX.Element {
             </CardActions>
           </CardContent>
         </Card>
-      </Box>
+      </Box> */}
     </div>
   );
 }
